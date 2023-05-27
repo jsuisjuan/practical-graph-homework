5126f293982e8b7257ea8592e5e6dd3c6e973b1e
jsuisjuan <juanc.s.delima@gmail.com>
2023-05-27 19:46:20 -0300

    add header system to the repository

---
+++ implementation-header.cpp
@@ -0,0 +1,5 @@
+#include "./header-files/header-graph.h"
+
+const char* header_test(){
+    return "just a header test";
+}
--- library.cpp
+++ library.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "implementation-header.cpp"
 using namespace std;
 /*
     Tasks:
     - implementar busca em largura e profundidade
 */
 int main(int argc,char *argv[]){
-    cout<<"This is the library!"<<endl;
+    cout<< header_test()<<endl;
     return 0;
 }
